f7c8f4bdbf251ad50d024459af5b9f2f3e3e9a7e
nifemibosun <nifemibosun70@gmail.com>
2024-08-14 15:51:04 -0700

    Just tested all apis and they are fine

--- backend/servers/blockchainServer.js
+++ backend/servers/blockchainServer.js
@@ -3,27 +3,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const minerBlockChainAddress = 'BlockChainminerBlockChainAddress';
-
-const bytechain = new BlockChain(minerBlockChainAddress);
+const bytechain = new BlockChain();
 const miningTimer = 10000;
+
+const minerBlockChainAddress = bytechain.minerBlockChainAddress;
+const GenesisTransactionRecipient = '1J8uVRSxixFcRFUjKPJNRkNhTev5dpsHXi'; 
 
 app.use(express.json())
 
 app.get('/blockchain', (req, res) => {
-    res.send(bytechain.chain);
+    res.status(200).send(bytechain.chain);
 });
 
-app.post('/transaction', (req, res) => {
-    res.send(bytechain.transactionPool);
+app.get('/blockchain/transactions', (req, res) => {
+    res.status(200).send(bytechain.transactionPool);
 });
 
 setInterval(() => {
     try {
+        // const newTrx = bytechain.AddNewTransaction()
         bytechain.Mine();
         console.log('Blockchain:', bytechain.chain);
         // Be cautious with sensitive data; ensure this is safe for logging
         console.log('Balance:', bytechain.CalculateBalance(minerBlockChainAddress));
+        console.log('Balance:', bytechain.CalculateBalance(GenesisTransactionRecipient));
+        
     } catch (error) {
         console.error('Error during mining or balance calculation:', error);
     } 
