2c6ab1e373820327ddd6a19de4f5c4855b573e9b
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2024-08-14 16:43:26 +0100

    Created Initial nav and header with no functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import { useState } from "react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
 
 function App() {
+  let [isLightmode, setisLightmode] = useState(true);
+
+  const toggleDarkMode = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    setisLightmode(newTheme === "light");
+    document.documentElement.setAttribute("data-theme", newTheme);
+  };
+
+  let modeIcon = isLightmode ? <MdLightMode /> : <MdDarkMode />;
+
   return (
     <div className="App">
-      
+      <Navbar onclick={toggleDarkMode} modeIcon={modeIcon} />
+      <Header />
     </div>
   );
 }
